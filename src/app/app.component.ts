a705ceaffe2349e48198f459d69b1b7f8a98e644
Mateusz Olszanecki <mateuszolszanecki@gmail.com>
2025-01-10 15:40:35 +0100

    Copying initial code from recruitment repository

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SvgNubisoftIconComponent } from './components/svg-nubisoft-icon/svg-nubisoft-icon.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [SvgNubisoftIconComponent],
 })
 export class AppComponent {}
